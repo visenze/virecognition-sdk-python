2bbc72cfc810103ee5f2eec1b8f6031fd3c39eb4
Han Xiaogang <xganghan@gmail.com>
2016-03-14 10:26:02 +0800

    add enquiry apis

--- virecognize/client.py
+++ virecognize/client.py
@@ -1,15 +1,16 @@
 from requests.auth import HTTPBasicAuth
-from .bind import bind_method, build_parameters
+from .bind import bind_method
 
 
 class ViRecognitionAPI(object):
     def __init__(self, access_key, secret_key):
-        self.host = "http://virecognition.visenze.com/"
+        # self.host = "http://virecognition.visenze.com/"
+        self.host = 'http://119.81.18.174:8082/'
         self.access_key = access_key
         self.secret_key = secret_key
         self.auth_info = HTTPBasicAuth(self.access_key, self.secret_key)
 
-    def recognition(self, url, tag_group):
+    def image_recognize(self, url, tag_group):
         path = 'image/recognize'
         method = 'POST'
         raw_parameters = {
@@ -18,4 +19,26 @@ class ViRecognitionAPI(object):
         }
         # parameters = build_parameters(path, raw_parameters)
         resp = bind_method(self, path, method, raw_parameters)
+        return resp
+
+    def video_recognize(self, url, tag_group):
+        path = 'video/recognize_async'
+        method = 'POST'
+        raw_parameters = {
+            'url': url,
+            'tag_group': tag_group
+        }
+        # parameters = build_parameters(path, raw_parameters)
+        resp = bind_method(self, path, method, raw_parameters)
+        return resp
+
+    def video_enquiry(self, transaction_id):
+        path = 'video/enquiry'
+        method = 'GET'
+        raw_parameters = {
+            'transaction_id': transaction_id
+        }
+        # parameters = build_parameters(path, raw_parameters)
+        resp = bind_method(self, path, method, raw_parameters)
+
         return resp
