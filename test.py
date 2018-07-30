9e7f309235be8fe6ed85660f3aecf288bcf35472
bo-git <liubo@visenze.com>
2018-07-30 17:07:22 +0800

    update endpoints

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ def run():
     api = ViRecognitionAPI(access_key, secret_key)
 
     image_url = 'http://sg-live-02.slatic.net/p/cchappiness-v-neck-short-sleeve-t-shirt-army-green-export-6879-861477-1-zoom.jpg'
-    tag_group = 'product'
+    tag_group = 'fashion_attributes'
 
     response = api.image_recognize(image_url, tag_group)
 
